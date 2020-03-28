b7c79e4c149064846ca15eee86553bde35e2a50f
Ian Hunter <ianfhunter@gmail.com>
2020-03-28 08:27:04 +0000

    Update setup_antlr.sh

--- setup_antlr.sh
+++ setup_antlr.sh
@@ -4,8 +4,13 @@ export CLASSPATH=".:external/antlr-$1-complete.jar:$CLASSPATH"
 
 shopt -s expand_aliases
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
-echo $DIR
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 alias antlr4='java -Xmx500M -cp "$DIR/external/antlr-$1-complete.jar:$CLASSPATH" org.antlr4.v4.Tool'
 
