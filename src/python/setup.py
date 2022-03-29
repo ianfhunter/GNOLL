60029ef892bdd5b92218ce474bbe851045da2d39
Ian Hunter <ianfhunter@gmail.com>
2022-03-29 07:53:05 +0100

    Create setup.py

---
+++ src/python/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='dicetower',
+    version='0.1.0',    
+    description='A comprehensive dice notation parser',
+    url='https://github.com/ianfhunter/DiceTower',
+    author='Ian Hunter',
+    author_email='ianfhunter@gmail.com',
+    license='GPL v3',
+    packages=['dicetower'],
+    install_requires=[
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
