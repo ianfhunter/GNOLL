fb474a0aa5521900b2bd3b212b87b27ddc03e049
Ian Hunter <ianfhunter@gmail.com>
2022-04-14 21:06:09 +0100

    Update parser.py

--- src/python/code/dicetower/parser.py
+++ src/python/code/dicetower/parser.py
@@ -9,20 +9,21 @@ from importlib import reload
 import tempfile
 
 # BUILD_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../build'))
-BUILD_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'c_build'))
-C_HEADER = os.path.join(os.path.dirname(__file__), 'c_includes')
-C_SHARED_LIB = os.path.join(BUILD_DIR, 'dice.so')
+BUILD_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "c_build"))
+C_HEADER = os.path.join(os.path.dirname(__file__), "c_includes")
+C_SHARED_LIB = os.path.join(BUILD_DIR, "dice.so")
 cppyy.c_include(os.path.join(C_HEADER, "shared_header.h"))
 cppyy.load_library(C_SHARED_LIB)
 
 
 def roll(s, verbose=False, mock=None, quiet=True, mock_const=3):
-    if verbose: print("Rolling: ", s)
-
+    if verbose: 
+        print("Rolling: ", s)
 
     temp = tempfile.NamedTemporaryFile(prefix="dicetower_roll_", suffix=".die")
     f = str(temp.name)
-    if verbose: print("File: ", f)
+    if verbose: 
+        print("File: ", f)
 
     cppyy.gbl.reset()
 
@@ -53,7 +54,7 @@ def roll(s, verbose=False, mock=None, quiet=True, mock_const=3):
     return int(return_code), out
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     arg = "".join(sys.argv[1:])
     arg = arg if arg != "" else "1d20"
     code, r = roll(arg, verbose=True)
