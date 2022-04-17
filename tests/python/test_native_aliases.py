956be166f958c5917e4960021c4a368c04243b94
Ian Hunter <ianfhunter@gmail.com>
2022-04-17 13:12:46 +0100

    Update test_native_aliases.py

--- tests/python/test_native_aliases.py
+++ tests/python/test_native_aliases.py
@@ -5,10 +5,12 @@ from util import roll, Mock
 
 # TODO: better tests for percentage dice - not fool proof
 
+
 def test_percentile_dice():
     result = roll("d%")
     assert result > 0
     assert result < 100
+
 
 def test_percentile_dice():
     result = roll("d%%2")
