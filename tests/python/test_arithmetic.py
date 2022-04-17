397a8979c54e336825022ff63957d726e5126603
Ian Hunter <ianfhunter@gmail.com>
2022-04-17 13:57:31 +0100

    Update test_arithmetic.py

--- tests/python/test_arithmetic.py
+++ tests/python/test_arithmetic.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
-from unittest import mock
 import pytest
-import csv
-import os
-import sys
 from util import roll, Mock
+
 
 @pytest.mark.parametrize("r,out",[
     ("4", 4),
@@ -14,7 +11,8 @@ from util import roll, Mock
 def test_numbers(r, out):
     # Just Numbers
     result = roll(r)
-    assert(result == out)
+    assert result == out
+
 
 @pytest.mark.parametrize("r,out,mock",[
     ("4+3", 7, Mock.NO_MOCK),
@@ -25,7 +23,8 @@ def test_numbers(r, out):
 ])
 def test_addition(r, out, mock):
     result = roll(r, mock_mode=mock)
-    assert(result == out)
+    assert result == out
+
 
 @pytest.mark.parametrize("r,out,mock",[
     ("-d40", -3, Mock.RETURN_CONSTANT),
@@ -39,7 +38,8 @@ def test_addition(r, out, mock):
 ])
 def test_subtraction(r, out, mock):
     result = roll(r, mock_mode=mock)
-    assert(result == out)
+    assert result == out
+
 
 @pytest.mark.parametrize("r,out,mock",[
     ("1d4*2", 6, Mock.RETURN_CONSTANT),
@@ -47,7 +47,8 @@ def test_subtraction(r, out, mock):
 ])
 def test_multiplication(r, out, mock):
     result = roll(r, mock_mode=mock)
-    assert(result == out)
+    assert result == out
+
 
 @pytest.mark.parametrize("r,out,mock",[
     ("1d4/2", 1, Mock.RETURN_CONSTANT),    # Down
@@ -55,7 +56,8 @@ def test_multiplication(r, out, mock):
 ])
 def test_division(r, out, mock):
     result = roll(r, mock_mode=mock)
-    assert(result == out)
+    assert result == out
+
 
 @pytest.mark.parametrize("r,out,mock",[
     ("1d4%2", 1, Mock.RETURN_CONSTANT),
@@ -63,4 +65,4 @@ def test_division(r, out, mock):
 ])
 def test_modulo(r, out, mock):
     result = roll(r, mock_mode=mock)
-    assert(result == out)
+    assert result == out
