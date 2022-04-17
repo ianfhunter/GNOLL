1c9007f121b03d85fad52f7608128088e37b393e
Ian Hunter <ianfhunter@gmail.com>
2022-04-17 14:18:14 +0100

    Update test_package.py

--- tests/python/test_package.py
+++ tests/python/test_package.py
@@ -1,12 +1,9 @@
 import pytest
-import csv
-import os
-import sys
 from util import roll
 
 def test_pip_package():
     from dicetower.parser import roll
 
     err_code, result = roll("1d4")
-    assert(err_code == 0)
-    assert(result in [1,2,3,4])
+    assert err_code == 0
+    assert result in [1,2,3,4]
