9c39be8979911772c26582576daf5b3c24a7ad51
Ian Hunter <ianfhunter@gmail.com>
2022-09-04 08:08:41 +0100

    Update and rename tests/python/test_dungeons_dragons_5E.py to tests/python/systems/dnd5E/test_5E.py
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- tests/python/systems/dnd5E/test_5E.py
+++ tests/python/systems/dnd5E/test_5E.py
@@ -4,8 +4,13 @@ import pytest
 from util import roll, Mock
 
 def test_5e_roll_for_character_stats():
+    # TODO: x6, for all stats
     result = roll("4d6kh3")
     assert result >= 3
     assert result <= 18
 
-#Todo x6, for all stats 
+def test_spell_attack():
+    # TODO: Versus DC
+    result = roll("1d20+2+4")
+    assert result >= 7
+    assert result <= 26
