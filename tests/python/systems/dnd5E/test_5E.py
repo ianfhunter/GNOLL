c6ddd5e737ff1eabd5dd0aee0eea1d1683d121b2
Ian Hunter <ianfhunter@gmail.com>
2022-09-04 08:13:09 +0100

    Update test_5E.py
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- tests/python/systems/dnd5E/test_5E.py
+++ tests/python/systems/dnd5E/test_5E.py
@@ -9,8 +9,15 @@ def test_5e_roll_for_character_stats():
     assert result >= 3
     assert result <= 18
 
-def test_spell_attack():
+def test_5e_spell_attack():
     # TODO: Versus DC
     result = roll("1d20+2+4")
     assert result >= 7
     assert result <= 26
+
+def test_5e_roll_hp():
+    # For a level 2 character
+    # TODO: ensure minimum roll of 1
+    result = roll("5+2+d8+2")
+    assert result >= 10
+    assert result <= 17
