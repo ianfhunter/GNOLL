50629acd0e0ba432a9a2636879c5b510a0e65232
Ian Hunter <ianfhunter@gmail.com>
2022-09-27 22:31:44 +0100

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/python/target.mk
+++ src/python/target.mk
@@ -3,27 +3,27 @@
 pip : all
 	echo "----------------- BUILD -------------------------"
 	# Copy Build
-	rm -rf src/python/code/gnoll/c_build/
-	rm -rf src/python/code/gnoll/c_includes/
-	rm -rf src/python/code/gnoll.egg-info/
+	rm -rf code/gnoll/c_build/
+	rm -rf code/gnoll/c_includes/
+	rm -rf code/gnoll.egg-info/
 
-	cp -r build/  src/python/code/gnoll/c_build/
-	cp -r src/grammar/  src/python/code/gnoll/c_includes/
+	cp -r ../../build/  code/gnoll/c_build/
+	cp -r ../grammar/  code/gnoll/c_includes/
 
-	cd src/python/ ; python3 -m build
+	python3 -m build
 	echo "------------------INSTALL------------------------"
-	python3 -m pip install -vvv --user --find-links=src/python/dist/ --force-reinstall --ignore-installed gnoll
+	python3 -m pip install -vvv --user --find-links=dist/ --force-reinstall --ignore-installed gnoll
 	echo "-------------------- TEST ----------------------"
 	python3 -c "from gnoll import parser as dt; dt.roll('2')"
 
 publish: test
-	#twine upload --repository-url https://test.pypi.org/legacy/ src/python/dist/*
-	twine upload src/python/dist/*
+	#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+	twine upload dist/*
 
 python_clean: 
 # Python Packaging can create extraoneous files
-	rm src/python/src/ | true
-	rm src/python/dist/ | true
-	rm src/python/build/ | true
-	rm src/python/LICENSE | true
-	rm src/python/Makefile | true
+	rm src/ | true
+	rm dist/ | true
+	rm build/ | true
+	rm LICENSE | true
+	rm Makefile | true
