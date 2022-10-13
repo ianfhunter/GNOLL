a737bb34185ac4cdcb21bf21be13378826db601c
Ian Hunter <ianfhunter@gmail.com>
2022-10-13 08:34:03 +0100

    Update macro_logic.c
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/macro_logic.c
+++ src/grammar/macro_logic.c
@@ -14,14 +14,14 @@ void register_macro(vec * macro_name, vec *to_store) {
 
     char * skey = macro_name->symbols[0];
     int key = safe_strtol(skey, NULL, 10);
-    if(gnoll_errno) return;
+    if(gnoll_errno){return;}
 
     struct macro_struct *s;
 
     HASH_FIND_INT(macros, &key, s);  /* id already in the hash? */
     if (s == NULL){
         s = (struct macro_struct*)safe_malloc(sizeof *s);
-        if(gnoll_errno) return;
+        if(gnoll_errno){return;}
         s->id = key;
         HASH_ADD_INT(macros, id, s);  /* id: name of key field */
     }
