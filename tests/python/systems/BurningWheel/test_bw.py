c4503e3d942ea54969a966ed5e7cfe4225f6f73e
Ian Hunter <ianfhunter@gmail.com>
2022-10-14 08:38:39 +0100

    Fixes #209
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

---
+++ tests/python/systems/BurningWheel/test_bw.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import pytest
+from util import roll, Mock
+
+def test_bw_dsix_over_four():
+    # TODO: x6, for all stats
+    result = roll("6d6>=4c")
+    assert result >= 1
+    assert result <= 6
