961305a38602aec492614258b676de7344c7237f
Ian Hunter <ianfhunter@gmail.com>
2022-10-18 22:29:27 +0100

    Update macro_logic.h
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/macro_logic.h
+++ src/grammar/macro_logic.h
@@ -4,11 +4,10 @@
 
 #include "uthash.h"
 #include "vector_functions.h"
-#include "rolls/dice_rolls_structs.h"
+#include "rolls/dice_roll_structs.h"
 
 struct macro_struct {
     int id;                    /* key */
-    // char name[MAX_SYMBOL_LENGTH];
     roll_params stored_dice_roll;
     UT_hash_handle hh;         /* makes this structure hashable */
 };
