39076c1a5eefc21198b627ec0c37cae601ad6ec2
Ian Hunter <ian.hunter@intel.com>
2022-10-18 17:39:31 +0100

    fix test

--- tests/python/systems/BurningWheel/test_bw.py
+++ tests/python/systems/BurningWheel/test_bw.py
@@ -6,5 +6,5 @@ from util import roll, Mock
 def test_bw_dsix_over_four():
     # TODO: x6, for all stats
     result = roll("6d6f>=4c")
-    assert result >= 1
+    assert result >= 0
     assert result <= 6
