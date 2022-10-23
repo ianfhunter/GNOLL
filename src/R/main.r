9228b1aa4bd62b21860ce0eea7c89d539281100d
Ian Hunter <ianfhunter@gmail.com>
2022-10-23 13:37:17 +0100

    Update main.r
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/R/main.r
+++ src/R/main.r
@@ -1,1 +1,17 @@
 print("Hello GNOLL")
+
+library.dynam(
+    "dice.so", 
+    "GNOLL", 
+    "",
+    "../../build/"
+    verbose = True,
+    file.ext = .Platform$dynlib.ext
+)
+
+return_code <- GNOLL.roll_and_write(
+    "1d20",
+    "output.dice"
+)
+
+print(return_code)
