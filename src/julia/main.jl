3d68ba2653fe09bc3a72374122780bd1d2f963a3
Ian Hunter <ianfhunter@gmail.com>
2022-10-24 08:22:08 +0100

    Update main.jl
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/julia/main.jl
+++ src/julia/main.jl
@@ -2,7 +2,7 @@ using Printf
 const gnoll = joinpath(@__DIR__, "../../build/dice.so")
 gnoll_file = "jl.dice"
 exit_code = ccall((:roll_and_write, gnoll), Int, (Cstring, Cstring), "1d20", gnoll_file)
-@printf("Exit Code: %i", exit_code) 
+@printf("Exit Code: %i\n", exit_code) 
 f = open(gnoll_file, "r")
 while ! eof(f) 
      s = readline(f)         
