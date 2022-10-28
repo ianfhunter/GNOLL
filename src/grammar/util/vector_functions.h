566c7a299c24ac5fe3140028b70a739169807b50
Ian Hunter <ian.hunter@intel.com>
2022-10-28 12:08:46 +0100

    Selecting Google Code Style for C/C++

--- src/grammar/util/vector_functions.h
+++ src/grammar/util/vector_functions.h
@@ -1,39 +1,45 @@
 #ifndef VECFN_HEADER
 #define VECFN_HEADER
 
-#include "shared_header.h"
 #include "rolls/dice_enums.h"
 #include "rolls/vec.h"
+#include "shared_header.h"
 
-void initialize_vector(vec * vector, DIE_TYPE dt, unsigned int number_of_items);
-void light_initialize_vector(vec * vector, DIE_TYPE dt, unsigned int number_of_items);
+void initialize_vector(vec* vector, DIE_TYPE dt, unsigned int number_of_items);
+void light_initialize_vector(vec* vector, DIE_TYPE dt,
+                             unsigned int number_of_items);
 
-void pop(int * arr, unsigned int len, int value, int * new_arr);
+void pop(int* arr, unsigned int len, int value, int* new_arr);
 
-void abs_vec(vec * x);
+void abs_vec(vec* x);
 
-int contains(int * arr, unsigned int len, int value);
-int min_in_vec(int * arr, unsigned int len);
-int max_in_vec(int * arr, unsigned int len);
+int contains(int* arr, unsigned int len, int value);
+int min_in_vec(int* arr, unsigned int len);
+int max_in_vec(int* arr, unsigned int len);
 
-void concat_symbols(char ** arr1, unsigned int len1,
-                    char ** arr2, unsigned int len2,
-                    char ** new_arr);
+void concat_symbols(char** arr1, unsigned int len1, char** arr2,
+                    unsigned int len2, char** new_arr);
 
-void collapse_vector(vec * vector, vec * new_vector);
+void collapse_vector(vec* vector, vec* new_vector);
 
-void keep_logic(vec * vector, vec * new_vector, unsigned int number_to_keep, int keep_high);
-void keep_highest_values(vec * vector, vec * new_vector, unsigned int number_to_keep);
-void keep_lowest_values(vec * vector, vec * new_vector, unsigned int number_to_keep);
-void drop_highest_values(vec * vector, vec * new_vector, unsigned int number_to_keep);
-void drop_lowest_values(vec * vector, vec * new_vector, unsigned int number_to_keep);
+void keep_logic(vec* vector, vec* new_vector, unsigned int number_to_keep,
+                int keep_high);
+void keep_highest_values(vec* vector, vec* new_vector,
+                         unsigned int number_to_keep);
+void keep_lowest_values(vec* vector, vec* new_vector,
+                        unsigned int number_to_keep);
+void drop_highest_values(vec* vector, vec* new_vector,
+                         unsigned int number_to_keep);
+void drop_lowest_values(vec* vector, vec* new_vector,
+                        unsigned int number_to_keep);
 
-void extract_symbols(char ** symbols_list, char ** result_symbols, int * indexes, unsigned int idx_length);
+void extract_symbols(char** symbols_list, char** result_symbols, int* indexes,
+                     unsigned int idx_length);
 
 void print_vec(vec vector);
 
-void filter(vec * dice, vec * cond, int comparitor, vec * output);
+void filter(vec* dice, vec* cond, int comparitor, vec* output);
 
-void filter_unique(vec * dice, vec * new_vec);
+void filter_unique(vec* dice, vec* new_vec);
 
 #endif
--- src/grammar/yacc_header.h
+++ src/grammar/yacc_header.h
@@ -8,8 +8,8 @@
 int initialize();
 // int resolve_dice(dice die);
 
-int sum(int * arr, unsigned int len);
-int collapse(int * arr, unsigned int len);
+int sum(int* arr, unsigned int len);
+int collapse(int* arr, unsigned int len);
 int roll_numeric_die(int small, int big);
 int roll_symbolic_die(unsigned int length_of_symbolic_array);
 
