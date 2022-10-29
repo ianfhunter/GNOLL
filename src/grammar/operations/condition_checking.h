1ce8c8333a04a74cd260b8b9347c5a6e12af76e9
Ian Hunter <ianfhunter@gmail.com>
2022-10-29 11:38:49 +0100

    Update condition_checking.h
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/operations/condition_checking.h
+++ src/grammar/operations/condition_checking.h
@@ -12,10 +12,14 @@ typedef enum {
   GREATER_OR_EQUALS = 4,
   LESS_OR_EQUALS = 5,
   NOT_EQUAL = 6,
-  UNIQUE = 7
+  UNIQUE = 7,
+  IF_EVEN = 8,
+  IF_ODD = 9,
+  IF_SAME = 10,
 } COMPARATOR;
 
 int check_condition(vec* x, vec* y, COMPARATOR c);
 
 int check_condition_scalar(int x, int y, COMPARATOR c);
+int check_condition_vector(vec* v, COMPARATOR c);
 #endif
