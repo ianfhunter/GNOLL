a7b52b740e0b52b1f3c696d286372399c17b43c6
Ian Hunter <ianfhunter@gmail.com>
2022-10-29 12:31:24 +0100

    Update test_filtering.py
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- tests/python/test_filtering.py
+++ tests/python/test_filtering.py
@@ -14,9 +14,9 @@ from util import Mock, roll
         ("10d10f>=8", 27, Mock.RETURN_INCREMENTING,
          1),  # equal or greater than
         ("10d10f<=3", 6, Mock.RETURN_INCREMENTING, 1),  # equal or less than
-        ("10d10feven", 5, Mock.RETURN_INCREMENTING, 1),  # even
-        ("10d10fodd", 5, Mock.RETURN_INCREMENTING, 1),  # odd
-        ("10d10fsame", 0, Mock.RETURN_INCREMENTING, 1),  # same
+        ("10d10fis_even", 5, Mock.RETURN_INCREMENTING, 1),  # even
+        ("10d10fis_odd", 5, Mock.RETURN_INCREMENTING, 1),  # odd
+        ("10d10fis_same", 0, Mock.RETURN_INCREMENTING, 1),  # same
     ],
 )
 def test_filter(r, out, mock, mock_const):
