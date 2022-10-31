ffaa2abfd7ca1309407996b627298d26b2b47916
Ian Hunter <ianfhunter@gmail.com>
2022-10-31 11:25:30 +0000

    Update yatzy.py
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- scripts/demo/yatzy.py
+++ scripts/demo/yatzy.py
@@ -14,6 +14,21 @@ def scorecard (dice):
 
     top_bonus = 50 if sum(top) >= 63 else 0
 
+    one_pair_sum = 0
+    two_pair_sum = 0
+    three_oak_sum = 0
+    four_oak_sum = 0
+    low_straight = 0
+    high_straight = 0
+    full_house = 0
+    chance = 0
+    five_oak = 0
+
+    if(all(dice[0] == dice)):
+        five_oak = 50
+    
+    # hello
+
     print(f"""
 Dice: {dice}
 
@@ -25,6 +40,18 @@ Dice: {dice}
 [6]: {top[5]}
 
 Top Bonus: {top_bonus}
+
+One Pair: {one_pair_sum}
+Two Pairs: {two_pair_sum}
+Three of a Kind: {three_oak_sum}
+Four of a Kind: {four_oak_sum}
+Small Straight: {low_straight}
+Large Straight: {high_straight} 
+Full House: {full_house}
+Chance: {chance}
+Yatzy: {five_oak}
+
+Total: {}
     """)
 
 
