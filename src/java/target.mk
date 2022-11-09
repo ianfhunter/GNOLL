490bae33ac463e112a373e2a6b0e82c34889538a
Ian Hunter <ianfhunter@gmail.com>
2022-11-09 23:31:21 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/java/target.mk
+++ src/java/target.mk
@@ -10,7 +10,7 @@ JAVA_HOME=$(shell dirname $$(readlink -f $$(which java))|sed 's/bin//')
 java: clean yacc lex compile $(OBJECTS)
 	mkdir -p build/java/
 	$(CC) $(SHAREDCFLAGS) -c src/java/src/org_gnoll_DiceNotationParser.c -I $(JAVA_HOME)/include -I $(JAVA_HOME)/include/linux
-	$(CC) -shared -o build/java/libdice.so $(OBJECTS) $(ARC4RANDOM) -Wl,--kill-at,-soname,gnoll
+	$(CC) -shared -o build/java/libdice.so $(OBJECTS) $(ARC4RANDOM) -Wl,-soname,gnoll
 	cp build/java/libdice.so  src/java/libdice.so
 	cd src/java/src/; LD_LIBRARY_PATH=$(PWD)/build/java/ javac -cp .:$(PWD)/src/java/classes/ -d $(PWD)/src/java/classes/ Test.java
 	cd src/java; LD_LIBRARY_PATH=$(PWD)/build/java/ java -cp .:$(PWD)/src/java/classes/ Test #test.java
