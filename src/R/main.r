69aac51082de2f9072dcd026f62eb5bfbbee17d5
Ian Hunter <ianfhunter@gmail.com>
2022-11-09 08:07:58 +0000

    Update main.r
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/R/main.r
+++ src/R/main.r
@@ -9,7 +9,7 @@ return_code <- .C("roll_and_write_2d_pointers",
 )
 if(return_code != 0){
     print(paste("GNOLL Error Code:", return_code))
-    stop("GNOLL Exit code was not Success")
+    #stop("GNOLL Exit code was not Success")
 )
 
 result <- read_lines("output.dice", skip = 0, n_max = -1)
