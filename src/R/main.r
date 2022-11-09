57be979ef36ed3421b2433040c807237369e22ce
Ian Hunter <ianfhunter@gmail.com>
2022-11-09 07:39:02 +0000

    Update main.r
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/R/main.r
+++ src/R/main.r
@@ -2,6 +2,7 @@ print("Hello GNOLL")
 
 dyn.load("build/r/dice.so")
 
+# TODO: delete file
 return_code <- .C("roll_and_write_2d_pointers",
     "1d20",
     "output.dice"
@@ -9,3 +10,10 @@ return_code <- .C("roll_and_write_2d_pointers",
 assert("GNOLL Exit code was SUCCESS", (return_code==0))
 
 print(return_code)
+
+result <- read_lines("output.dice, skip = 0, n_max = -1)
+result <- strtol(result)
+
+assert("GNOLL rolled valid value", (result > 0), (result <= 20))
+
+
