72292ea44b4db96dce1b4b0be393c0ed915c168a
Ian Hunter <ian.hunter@intel.com>
2022-11-10 10:47:28 +0000

    working java

--- src/java/src/Test.java
+++ src/java/src/Test.java
@@ -1,32 +1,33 @@
 import org.gnoll.DiceNotationParser;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.*;
-import java.io.IOException;
+import java.io.*;
 
 public class Test {
     public static void main(String[] args) {
         String fn = "tmp.dice";
-        File myObj = new File(fn); 
-        myObj.delete();
-        int r = DiceNotationParser.roll("1d200",fn);
+        Path fp = Paths.get(fn);
+        File f =  new File(fn);;
 
-        System.out.println("GNOLL RC:"+r);
-        assert r == 0;
-        
-        Path path = Paths.get(path);
-        List<String> lines;
+        f.delete();
         try{
-            lines = Files.readAllLines(fn); //path);
+            f.createNewFile();
         }
         catch(IOException e) {
-            lines = Collections.<String>emptyList();
             assert false;
         }
-        assert lines.size() > 0 ;
-        System.out.println("GNOLL Rolled:" + lines.get(0));
+        
+        int r = DiceNotationParser.roll("1d200", fn);
+        System.out.println("GNOLL RC:"+r);
+        assert r == 0;
 
+        try{
+            List<String> lines = Files.readAllLines(fp);
+            assert lines.size() > 0 ;
+            System.out.println("GNOLL Rolled:" + lines.get(0));
+        }
+        catch(IOException e) {
+            assert false;
+        }
     }
 }
--- src/java/src/org_gnoll_DiceNotationParser.c
+++ src/java/src/org_gnoll_DiceNotationParser.c
@@ -7,7 +7,7 @@ JNIEXPORT jint JNICALL Java_org_gnoll_DiceNotationParser_roll
   (JNIEnv* env, jclass jc, jstring js, jstring fn)
 {
     char* dieString = (char *)(*env)->GetStringUTFChars(env, js, JNI_FALSE);
-    char* fileName = (char *)(*env)->GetStringUTFChars(env, js, JNI_FALSE);
+    char* fileName = (char *)(*env)->GetStringUTFChars(env, fn, JNI_FALSE);
     int return_code = roll_and_write(dieString, fileName);
     return return_code; 
 }
