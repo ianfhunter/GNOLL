8ee246b451d6e168e198f0efc3081adc7cb0e44d
Ian Hunter <ian.hunter@intel.com>
2022-11-10 10:53:36 +0000

    remove gend header

--- src/java/target.mk
+++ src/java/target.mk
@@ -9,7 +9,7 @@ JAVA_FOLDER=$(PWD)/src/java
 
 java: clean yacc lex compile $(OBJECTS)
 	mkdir -p build/java/
-	cd $(JAVA_FOLDER) ; javac src/org/gnoll/DiceNotationParser.java  -h .; cd ../..
+	cd $(JAVA_FOLDER)/src ; javac org/gnoll/DiceNotationParser.java  -h .; cd ../..
 	$(CC) $(SHAREDCFLAGS) -c src/java/src/org_gnoll_DiceNotationParser.c -I $(JAVA_HOME)/include -I $(JAVA_HOME)/include/linux
 	$(CC) -shared -o build/java/libdice.so org_gnoll_DiceNotationParser.o $(OBJECTS) $(ARC4RANDOM) -Wl,-soname,gnoll
 	cp build/java/libdice.so  src/java/libdice.so
