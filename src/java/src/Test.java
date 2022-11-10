127365faf1fad69ac1bec94e6af77ab1127abf01
Ian Hunter <ianfhunter@gmail.com>
2022-11-10 01:16:36 +0000

    Update Test.java
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/java/src/Test.java
+++ src/java/src/Test.java
@@ -1,8 +1,16 @@
 import org.gnoll.DiceNotationParser;
 public class Test {
     public static void main(String[] args) {
-        int r = DiceNotationParser.roll("1d200");
+        String fn = "tmp.dice";
+        File myObj = new File(fn); 
+        myObj.delete()
+        int r = DiceNotationParser.roll("1d200",fn);
 
         System.out.println("GNOLL RC:"+r);
+        assert r == 0;
+        
+        Path path = Paths.get(fn);
+        List<String> lines = Files.readAllLines(path);
+        System.out.println("GNOLL Rolled:", lines[0]);
     }
 }
