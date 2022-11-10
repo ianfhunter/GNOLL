d27767ea078288c9a8515b7607c507d2bc86a54a
Ian Hunter <ianfhunter@gmail.com>
2022-11-10 01:18:30 +0000

    Update org_gnoll_DiceNotationParser.c
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/java/src/org_gnoll_DiceNotationParser.c
+++ src/java/src/org_gnoll_DiceNotationParser.c
@@ -4,9 +4,10 @@
 #include <jni.h>
 
 JNIEXPORT jint JNICALL Java_org_gnoll_DiceNotationParser_roll
-  (JNIEnv* env, jclass jc, jstring  js)
+  (JNIEnv* env, jclass jc, jstring js, jstring fn)
 {
     char* dieString = (char *)(*env)->GetStringUTFChars(env, js, JNI_FALSE);
-    int return_code = roll_and_write(dieString, "output.dice");
-    return return_code; //(*env)->NewIntArray(env, return_code);
+    char* fileName = (char *)(*env)->GetStringUTFChars(env, js, JNI_FALSE);
+    int return_code = roll_and_write(dieString, fileName);
+    return return_code; 
 }
