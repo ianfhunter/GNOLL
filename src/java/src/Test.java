067409aa180d00ffb3d68dfdf0dab086f23bbf18
Ian Hunter <ianfhunter@gmail.com>
2022-11-10 01:34:10 +0000

    Update Test.java
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/java/src/Test.java
+++ src/java/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
     public static void main(String[] args) {
         String fn = "tmp.dice";
         File myObj = new File(fn); 
-        myObj.delete()
+        myObj.delete();
         int r = DiceNotationParser.roll("1d200",fn);
 
         System.out.println("GNOLL RC:"+r);
