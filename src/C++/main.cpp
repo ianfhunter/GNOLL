0c566ad4b0c4ff7659f0e943860ecb519f198423
Ian Hunter <ianfhunter@gmail.com>
2022-11-13 10:10:33 +0000

    Create main.cpp
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

---
+++ src/C++/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "shared_header.h"
+
+int roll_full_options(
+    char* roll_request, 
+    char* log_file, 
+    int enable_verbosity, 
+    int enable_introspection,
+    int enable_mocking,
+    int mocking_type,
+    int mocking_seed
+);
+
+int main()
+{
+  const char* fn = "out.dice";
+  
+  remove(fn);
+
+  int err_code = roll_full_options(
+    "1d20",
+    "out.dice",
+    0,
+    0,
+    0,
+    0,
+    0
+  );
+  std::ifstream myfile; 
+  myfile.open(fn);
+  std::string mystring;
+  if ( myfile.is_open() ) { 
+    myfile >> mystring;
+    std::cout << mystring; 
+  }else{
+    return 1;
+  }
+  return 0;
+}
