f72e5c9615411badf275f84d74e912b33824b631
Ian Hunter <ianfhunter@gmail.com>
2022-11-14 23:08:52 +0000

    Update macro_logic.c
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/operations/macro_logic.c
+++ src/grammar/operations/macro_logic.c
@@ -74,6 +74,7 @@ void register_macro(vec *macro_name, roll_params *to_store) {
         &s->stored_dice_roll.symbol_pool, to_store->symbol_pool,
         to_store->die_sides, MAX_SYMBOL_LENGTH);
   }
+  free(s); //new
 }
 
 void search_macros(char *skey, roll_params *to_store) {
