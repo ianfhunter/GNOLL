98976767c72c610d59e47ed4c19d06599c8a8b31
Ian Hunter <ianfhunter@gmail.com>
2022-11-15 08:36:37 +0000

    Update macro_logic.c
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/operations/macro_logic.c
+++ src/grammar/operations/macro_logic.c
@@ -72,9 +72,9 @@ void register_macro(vec *macro_name, roll_params *to_store) {
   if (is_symbolic) {
     free_2d_array(&s->stored_dice_roll.symbol_pool,
                   s->stored_dice_roll.die_sides);
-   // safe_copy_2d_chararray_with_allocation(
-   //     &s->stored_dice_roll.symbol_pool, to_store->symbol_pool,
-   //    to_store->die_sides, MAX_SYMBOL_LENGTH);
+   safe_copy_2d_chararray_with_allocation(
+        &s->stored_dice_roll.symbol_pool, to_store->symbol_pool,
+       to_store->die_sides, MAX_SYMBOL_LENGTH);
   }
   
   //free(s); //new
