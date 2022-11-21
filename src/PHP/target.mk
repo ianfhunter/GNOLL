124555a7a20704363f3a7c8ce573ea876fb7a509
Ian Hunter <ianfhunter@gmail.com>
2022-11-21 17:54:55 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/PHP/target.mk
+++ src/PHP/target.mk
@@ -4,5 +4,5 @@ PHP_FOLDER:= /usr/lib/php/20210902/
 php: clean yacc lex compile shared
 	cp build/dice.so src/PHP/dice.so
 	echo ${PHP_FOLDER}
-	cp build/dice.so ${PHP_FOLDER}/dice.so
+	sudo cp build/dice.so ${PHP_FOLDER}/dice.so
 	cd src/PHP/ && php index.php
