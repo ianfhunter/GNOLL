3ba9131b403ad64ba45e7dfa55f14d7586ad3625
Ian Hunter <ianfhunter@gmail.com>
2022-11-28 22:24:57 +0000

    Create main.rb
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

---
+++ src/ruby/main.rb
@@ -0,0 +1,16 @@
+require 'ffi'
+
+module GNOLL
+  extend FFI::Library
+
+  ffi_lib '../../build/dice.so'
+  attach_function :roll_and_write, [:string, :string], :void
+end
+
+module DiceNotation
+  def self.roll(roll_str)
+     err_code = GNOLL.roll_and_write(roll_str, "output.dice")
+  end
+end
+
+DiceNotation.roll("1d20")
