4ada739cf501174e1ad8cb6b1553875dce7ce467
Ian Hunter <ian.hunter@intel.com>
2022-11-29 11:18:06 +0000

    add end

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -9,17 +9,19 @@ end
 
 module DiceNotation
   def self.roll(roll_str)
-     # Clear File
-     df = File.open('output.dice', 'r') do |f|
-     File.delete(f)
+     filename = 'output.dice'
+
+     # Clear File to avoid error
+     File.delete(filename) if File.exist?(filename)
 
      # GNOLL
-     err_code = GNOLL.roll_and_write(roll_str, "output.dice")
+     err_code = GNOLL.roll_and_write(roll_str, filename)
 
      # Read output
-     df = File.open("output.dice", "r")
-     data = df.read
-     puts data
+     df = File.open(filename, "r") do |f|
+       data = df.read
+       puts data
+     end
   end
 end
 
