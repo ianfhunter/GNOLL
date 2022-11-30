ac80fa157b825d705c5160ed6a0a6b023091e9fb
Ian Hunter <ianfhunter@gmail.com>
2022-11-30 13:06:01 +0000

    Update index.php
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/PHP/index.php
+++ src/PHP/index.php
@@ -2,7 +2,18 @@
 
 echo 'Hello, GNOLL!';
 
-if (!extension_loaded('dice')) {
-   dl('dice');
-}
-roll_and_write('d300', 'output.die');
+//if (!extension_loaded('dice')) {
+//  dl('dice');
+//}
+//roll_and_write('d300', 'output.die');
+
+
+
+// create FFI object
+$gnoll = FFI::cdef(
+    "int roll_and_write(char * roll, char *fn );", 
+    "libdice.so"
+);
+
+$gnoll->roll_and_write("3d6", "output.dice");
+
