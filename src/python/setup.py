4bc558e65c23c4b515eff0bbf172bc3a72d02a4d
Ian Hunter <ianfhunter@gmail.com>
2022-12-04 13:24:56 +0000

    Update setup.py
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup
 
-setup(
+#setup()
 
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+    class bdist_wheel(_bdist_wheel):
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            self.root_is_pure = False
+except ImportError:
+    bdist_wheel = None
+
+setup(
+    # ...
+    cmdclass={'bdist_wheel': bdist_wheel},
 )
