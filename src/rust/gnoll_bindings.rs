ed2d969b85b72eccd67ae0f82d815e41e571d9d8
Ian Hunter <ianfhunter@gmail.com>
2022-12-17 11:26:59 +0000

    Update gnoll_bindings.rs
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/rust/gnoll_bindings.rs
+++ src/rust/gnoll_bindings.rs
@@ -1,8 +1,13 @@
 #[repr(C)]
 
 extern "C" {
-    pub fn roll_and_wrie(
+    pub fn roll_and_write(
         die: *cty::c_char,
         fp: *cty::c_char
     );
+}
+
+fn main() {
+    unsafe { roll_and_write("10d20") }
+    println!("Hello, world!");
 }
