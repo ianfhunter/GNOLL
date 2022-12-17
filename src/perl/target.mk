c6da7b8c215c6b8f252eecab25be1cb8b8629eb1
Ian Hunter <ianfhunter@gmail.com>
2022-12-17 10:28:54 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/perl/target.mk
+++ src/perl/target.mk
@@ -6,7 +6,7 @@ swig_perl: clean yacc lex compile $(OBJECTS)
 
 PERL_VERSION=5.30
 compile_perl: swig_perl 
-	$(CC) -fPIC -c build/perl/gnoll_wrap.c -I/usr/lib/x86_64-linux-gnu/perl/$PERL_VERSION/CORE/ -Dbool=char -Doff64_t=__off64_t -o build/gnoll_perl_wrap.o
+	$(CC) -fPIC -c build/perl/gnoll_wrap.c -I/usr/lib/x86_64-linux-gnu/perl/$(PERL_VERSION)/CORE/ -Dbool=char -Doff64_t=__off64_t -o build/gnoll_perl_wrap.o
 	$(CC) -shared -o build/gnoll.so build/gnoll_perl_wrap.o $(OBJECTS)
 
 perl: compile_perl
