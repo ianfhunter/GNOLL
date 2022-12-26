c938f70e4f3626686f8970aa1bdc189423b48a20
Ian Hunter <ianfhunter@gmail.com>
2022-12-26 10:36:29 +0000

    Update index.php
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/PHP/index.php
+++ src/PHP/index.php
@@ -13,6 +13,6 @@ unlink($fn);
 $err = $gnoll->roll_and_write("3d6", $fn);
 
 $myfile = fopen($fn, "r") or die("Unable to open file!");
-echo fread($myfile,filesize($fn));
+echo fread($myfile, filesize($fn));
 fclose($myfile);
 
