c4062ee3bbc581ba81687c246252b0f267c4998b
Ian Hunter <ianfhunter@gmail.com>
2022-12-29 08:21:38 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/go/target.mk
+++ src/go/target.mk
@@ -2,4 +2,6 @@
 .PHONY: go
 go: all
 	cp build/dice.so build/libdice.so 
+        ln -s build src/go/c_build/
+        ln -s src/grammar/ src/go/c_includes
 	cd src/go && LD_LIBRARY_PATH="${PWD}/build:${LD_LIBRARY_PATH}"  go build main.go && LD_LIBRARY_PATH="${PWD}/build:${LD_LIBRARY_PATH}"  go run main.go && cd ../..
