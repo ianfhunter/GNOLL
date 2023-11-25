368d96f4ed9f6e64807c0a804b86c9934790845e
Ian Hunter <ianfhunter@gmail.com>
2023-11-25 14:27:12 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/rust/target.mk
+++ src/rust/target.mk
@@ -1,5 +1,5 @@
 .PHONY: rust
 
 rust: all
-	cd src/rust && LD_LIBRARY_PATH=$(PWD)/../../build/:/home/runner/work/GNOLL/GNOLL/build/ cargo build -v
-	cd src/rust && LD_LIBRARY_PATH=$(PWD)/../../build/:/home/runner/work/GNOLL/GNOLL/build/ cargo run 
+	cd src/rust && LD_LIBRARY_PATH=$(PWD)/../../build/ cargo build -v
+	cd src/rust && LD_LIBRARY_PATH=$(PWD)/../../build/ cargo run 
