d38611e0e6b48cf66d3b15ad625ebb21f9db9d86
Ian Hunter <ianfhunter@gmail.com>
2023-11-25 15:06:33 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/rust/target.mk
+++ src/rust/target.mk
@@ -1,8 +1,9 @@
 .PHONY: rust
 
 rust: all
-	echo "ONE:" && ls $(PWD)/build/
+	echo "ONE:" && echo $LD_LIBRARY_PATH
 	echo "TWO;" && ls $(PWD)/build/libdice.so
-	cd src/rust && LD_LIBRARY_PATH=$(PWD)/build/ ldd $(PWD)/build/libdice.so
-	cd src/rust && LD_LIBRARY_PATH=$(PWD)/build/ cargo build -v
-	cd src/rust && LD_LIBRARY_PATH=$(PWD)/build/ cargo run 
+	echo ld -ldice
+	cd src/rust && ldd $(PWD)/build/libdice.so
+	cd src/rust && cargo build -v
+	cd src/rust && cargo run 
