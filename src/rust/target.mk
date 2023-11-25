6c71b7787dc520f3d6857939457fb7880e75a75f
Ian Hunter <ianfhunter@gmail.com>
2023-11-25 15:43:04 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/rust/target.mk
+++ src/rust/target.mk
@@ -2,8 +2,8 @@
 
 rust: all
 	echo "LDPATH"
-	export LD_LIBRARY_PATH=$(PWD)/build/:$(LD_LIBRARY_PATH)
-	echo $(LD_LIBRARY_PATH)
+	export LD_LIBRARY_PATH=$(PWD)/build/:$LD_LIBRARY_PATH
+	echo $LD_LIBRARY_PATH
 	ls $(PWD)/build/libdice.so -las
 	ld -ldice
 	ldd $(PWD)/build/libdice.so
