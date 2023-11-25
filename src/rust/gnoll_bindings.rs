8642b49186b7136b0d534a86b16f2ca0db6ef731
Ian Hunter <ianfhunter@gmail.com>
2023-11-25 13:05:39 +0000

    Update gnoll_bindings.rs
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/rust/gnoll_bindings.rs
+++ src/rust/gnoll_bindings.rs
@@ -8,6 +8,28 @@ extern "C" {
 }
 
 fn main() {
-    unsafe { roll_and_write("10d20") }
-    println!("Hello, world!");
+    let die = "10d20\0".as_ptr() as *mut cty::c_char;
+    let fp = "output.txt\0".as_ptr() as *mut cty::c_char;
+    
+    unsafe { roll_and_write(die, fp) }
+
+    // Read the result from the memory pointed to by fp
+    let result_cstr = CStr::from_ptr(fp);
+    
+    if let Ok(result_str) = result_cstr.to_str() {
+        if let Ok(result_num) = result_str.parse::<u32>() {
+            if result_num > 1 {
+                 println!("Result is: {}", result_num);
+                 process::exit(0);
+            } else {
+                println!("Result is not greater than 1");
+            }
+        } else {
+            println!("Failed to parse the result as an integer");
+        }
+    } else {
+        println!("Failed to convert CStr to str");
+    }
+    process::exit(1);
+
 }
