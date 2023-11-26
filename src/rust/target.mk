a16e64a9365243682f90b1003021976d1fc09f95
Ian Hunter <ianfhunter@gmail.com>
2023-11-26 10:52:24 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/rust/target.mk
+++ src/rust/target.mk
@@ -5,14 +5,6 @@ rust: all
 	ls "$(shell pwd)/build/"
 	echo "explicit"
 	ld -L"$(shell pwd)/build/" -ldice
-	#export LD_LIBRARY_PATH=$(shell pwd)/build/ && ld -ldice
-	#echo "done"
-	#$(eval LD_LIBRARY_PATH := $(shell pwd)/build/)
-	#export LD_LIBRARY_PATH
-	#echo "ldpath is $(LD_LIBRARY_PATH)"
-	#ls "$(shell pwd)/build/libdice.so" -las
-	#ls "$(LD_LIBRARY_PATH)/libdice.so" -las
-	#ld -ldice
-	#ldd "$(shell pwd)/build/libdice.so"
-	cd src/rust && cargo build -v #  -L $(shell pwd)/../../build/
+	export LD_LIBRARY_PATH=$(shell pwd)/build/
+	cd src/rust && RUSTFLAGS="-C link-arg=-L$(shell pwd)/../../build/ cargo build 
 	cd src/rust && cargo run 
