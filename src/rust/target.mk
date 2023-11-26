93ca3b2741356aeaf7dcf9a69985a5db29b6fe16
Ian Hunter <ianfhunter@gmail.com>
2023-11-26 02:51:49 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/rust/target.mk
+++ src/rust/target.mk
@@ -3,6 +3,8 @@
 rust: all
 	echo "pwd is $(shell pwd)"
 	ls "$(shell pwd)/build/"
+	echo "explicit"
+	ld -L"$(shell pwd)/build/" -ldice
 	export LD_LIBRARY_PATH=$(shell pwd)/build/ && ld -ldice
 	echo "done"
 	$(eval LD_LIBRARY_PATH := $(shell pwd)/build/)
