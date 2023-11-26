6adecd5d77171e28b6554715ff385fa1d2788be1
Ian Hunter <ianfhunter@gmail.com>
2023-11-26 10:56:44 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/rust/target.mk
+++ src/rust/target.mk
@@ -6,5 +6,5 @@ rust: all
 	echo "explicit"
 	ld -L"$(shell pwd)/build/" -ldice
 	export LD_LIBRARY_PATH=$(shell pwd)/build/
-	cd src/rust && RUSTFLAGS="-C link-arg=-L$(shell pwd)/../../build/" cargo build 
+	cd src/rust && RUSTFLAGS="-C link-arg=-L$(shell pwd)/build/" cargo build 
 	cd src/rust && cargo run 
