2a0193ed8b14c985513077a795a97f84df407a16
Ian Hunter <ianfhunter@gmail.com>
2023-11-27 07:55:22 +0000

    Update main.m
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/MATLAB/main.m
+++ src/MATLAB/main.m
@@ -1,8 +1,28 @@
+% Load the shared library
+libraryPath = '../../build/libdice.so';  % Replace with the actual path to your library
+lib = dlopen(libraryPath, 1);
 
-f = "output.dice"
-delete(f)
+% Define the function signature
+returnType = 'void'; 
+argTypes = {'char*', 'char*'};
+roll_and_write = libpointer('string', '');
 
-roll_and_write("d20", f)
+% Call the function
+[param1, param2] = deal('20d8', 'output.txt'); 
+calllib(lib, 'roll_and_write', roll_and_write, param1, param2);
 
-data = textread(f,"%s")
-print("Result: %s\n", data)
+% Unload the library
+dlclose(lib);
+
+% read result
+outputFilePath = 'output.txt';  % Replace with the actual path to your output file
+result = dlmread(outputFilePath);
+
+% Check if the result is greater than 1
+if result > 1
+    disp('Result is greater than 1!');
+    exit(0)
+else
+    disp('Result is not greater than 1.');
+end
+exit(1)
