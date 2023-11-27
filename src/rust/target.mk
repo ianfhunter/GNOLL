8fa6b595177515ccc5e456c83d2ad74b7781f8fc
Ian Hunter <ianfhunter@gmail.com>
2023-11-27 01:19:15 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/rust/target.mk
+++ src/rust/target.mk
@@ -5,4 +5,4 @@ rust: all
 	export LIBRARY_PATH=$LD_LIBRARY_PATH
 	sudo cp $(shell pwd)/build/libdice.so /usr/local/lib/libdice.so
 	cd src/rust && cargo build 
-	cd src/rust && export LD_LIBRARY_PATH=$(shell pwd)/build/ cargo run --verbose 
+	cd src/rust && LD_LIBRARY_PATH=$(shell pwd)/build/ cargo run --verbose 
