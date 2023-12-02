6b33596d9fb3b7d07e5b63cc376bb1e9233f734c
Ian Hunter <ianfhunter@gmail.com>
2023-12-02 14:15:55 +0000

    Update test_reported_bugs.py
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- tests/python/test_reported_bugs.py
+++ tests/python/test_reported_bugs.py
@@ -8,9 +8,9 @@ def test_issue_444():
     issue reported that the built-in/mock combination produces a segmentation fault
     WARN: this is testing the pip library not the local one
     """
-    from gnoll import roll
+    from gnoll import roll as gnollroll
 
     with pytest.raises(Exception) as exc_info:
-        roll("1d6", builtins=True, mock=1)
+        gnollroll("1d6", builtins=True, mock=1)
 
     assert exc_info.type is None, f"Exception {exc_info.value} was raised unexpectedly"
