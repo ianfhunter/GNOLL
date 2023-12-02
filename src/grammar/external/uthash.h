a3bca1d44d84ee069193d469cb1b6ba91f67ac25
Ian Hunter <ianfhunter@gmail.com>
2023-12-02 12:42:47 +0000

    Update uthash.h
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/external/uthash.h
+++ src/grammar/external/uthash.h
@@ -730,37 +730,37 @@ typedef unsigned char uint8_t;
     switch (_hj_k) {                                                          \
       case 11:                                                                \
         hashv += ((unsigned)_hj_key[10] << 24); /* FALLTHROUGH */             \
-        fallthrough                                                           \
+        fallthrough;                                                          \
       case 10:                                                                \
         hashv += ((unsigned)_hj_key[9] << 16); /* FALLTHROUGH */              \
-        fallthrough                                                           \
+        fallthrough;                                                          \
       case 9:                                                                 \
         hashv += ((unsigned)_hj_key[8] << 8); /* FALLTHROUGH */               \
-        fallthrough                                                           \
+        fallthrough;                                                          \
       case 8:                                                                 \
         _hj_j += ((unsigned)_hj_key[7] << 24); /* FALLTHROUGH */              \
-        fallthrough                                                           \
+        fallthrough;                                                          \
       case 7:                                                                 \
         _hj_j += ((unsigned)_hj_key[6] << 16); /* FALLTHROUGH */              \
-        fallthrough                                                           \
+        fallthrough;                                                          \
       case 6:                                                                 \
         _hj_j += ((unsigned)_hj_key[5] << 8); /* FALLTHROUGH */               \
-        fallthrough                                                           \
+        fallthrough;                                                          \
       case 5:                                                                 \
         _hj_j += _hj_key[4]; /* FALLTHROUGH */                                \
-        fallthrough                                                           \
+        fallthrough;                                                          \
       case 4:                                                                 \
         _hj_i += ((unsigned)_hj_key[3] << 24); /* FALLTHROUGH */              \
-        fallthrough                                                           \
+        fallthrough;                                                          \
       case 3:                                                                 \
         _hj_i += ((unsigned)_hj_key[2] << 16); /* FALLTHROUGH */              \
-        fallthrough                                                           \
+        fallthrough;                                                           \
       case 2:                                                                 \
         _hj_i += ((unsigned)_hj_key[1] << 8); /* FALLTHROUGH */               \
-        fallthrough                                                           \
+        fallthrough;                                                          \
       case 1:                                                                 \
         _hj_i += _hj_key[0]; /* FALLTHROUGH */                                \
-        fallthrough                                                           \
+        fallthrough;                                                          \
       default:;                                                               \
     }                                                                         \
     HASH_JEN_MIX(_hj_i, _hj_j, hashv);                                        \
