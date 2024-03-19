84450e4924776154282b748bd01a397427c9b50d
Ian Hunter <ianfhunter@gmail.com>
2024-03-19 15:40:49 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/js/target.mk
+++ src/js/target.mk
@@ -6,7 +6,8 @@ javascript: clean yacc lex
 	mkdir -p build/js/
 	emcc $(JS_OPT) $(CFILES) \
 	$(CFLAGS) \
-        -o build/js/a.out.js -Wno-error=implicit-function-declaration -Wno-error=sign-conversion
+        -o build/js/a.out.js -Wno-error=implicit-function-declaration -Wno-error=sign-conversion -Wno-error=sign-compare
+# YACC/LEX generates code with errors, so disabling warning-to-error escalation
 
 js: javascript  
 # Make alias for ease of use
