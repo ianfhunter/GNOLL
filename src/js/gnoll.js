586d3744f53ea8cf2cab8abae642c06ef36ec9aa
Michael McDermott <mcdermott.michaelj@gmail.com>
2024-03-22 09:37:19 -0500

    Adding index file.

--- src/js/gnoll.js
+++ src/js/gnoll.js
@@ -1,20 +1,5 @@
 import gwFactory from './gnollwasm.js';
 
-//var Module = null; const modulePromise = await gwFactory().then(x => Module = x);
-
-/*
-export function roll(notation) {
-  Module.resetOut();
-
-  Module.cwrap('roll_full_options', 'number', ['string',
-      'string', 'number', 'number', 'number', 'number', 'number',
-      'number'],
-    [notation, null, 0, 0, 0, 1, 0, 0]
-  );
-
-  return Module.stdout;
-}
-*/
 export async function roll(notation) {
   return new Promise((resolve) => {
     gwFactory().then((Module) => {
@@ -32,6 +17,3 @@ export async function roll(notation) {
   });
 }
 
-export function temproll(notation) {
-  return "foobar";
-}
