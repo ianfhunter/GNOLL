bdc0e46c62a32f8dbe90581fdc4e4ccf5ab03e57
Ian Hunter <ianfhunter@gmail.com>
2024-03-23 23:30:36 +0000

    Update vec.h
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/constructs/vec.h
+++ src/grammar/constructs/vec.h
@@ -7,11 +7,12 @@
 
 typedef struct vec {
   DIE_TYPE dtype;
-  int* content;
+  union {
+    long long* content;
+    char** symbols;
+  }
   unsigned int length;
   // TODO: Split length into content_length and symbol length
-  //  maybe use union? If it exists in c
-  char** symbols;
   roll_params source;
   bool has_source;
 } vec;
