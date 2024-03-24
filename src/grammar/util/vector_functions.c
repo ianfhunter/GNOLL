c82c3f952188bb4d6aada6f23eedaa08da2bd7fc
Ian Hunter <ianfhunter@gmail.com>
2024-03-24 00:08:32 +0000

    Update vector_functions.c
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/util/vector_functions.c
+++ src/grammar/util/vector_functions.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "operations/conditionals.h"
 #include "shared_header.h"
@@ -13,7 +14,7 @@
 extern int gnoll_errno;
 
 void light_initialize_vector(vec *vector, DIE_TYPE dt,
-                             unsigned int number_of_items) {
+                             unsigned long long number_of_items) {
   /**
    * @brief Initializes a vector, but does not fill out 2d arrays
    * @param vector vector to initialize
@@ -26,13 +27,13 @@ void light_initialize_vector(vec *vector, DIE_TYPE dt,
   vector->has_source = false;
 
   if (dt == NUMERIC) {
-    vector->content = (int*)safe_calloc(number_of_items, sizeof(int));
+    vector->content = (long long*)safe_calloc(number_of_items, sizeof(long long));
     if (gnoll_errno) return;
   } else if (dt == SYMBOLIC) {
     vector->symbols = (char**)safe_calloc(1, sizeof(char **));
   }
 }
-void initialize_vector(vec *vector, DIE_TYPE dt, unsigned int number_of_items) {
+void initialize_vector(vec *vector, DIE_TYPE dt, unsigned long long number_of_items) {
   /**
    * @brief Initializes a vector, reserving space for 2d arrays
    * @param vector vector to initialize
@@ -49,7 +50,7 @@ void initialize_vector(vec *vector, DIE_TYPE dt, unsigned int number_of_items) {
   vector->has_source = false;
 
   if (dt == NUMERIC) {
-    vector->content = (int*)safe_calloc(number_of_items, sizeof(int));
+    vector->content = (long long*)safe_calloc(number_of_items, sizeof(long long));
     if (gnoll_errno) return;
   } 
   else if (dt == SYMBOLIC) 
@@ -57,15 +58,15 @@ void initialize_vector(vec *vector, DIE_TYPE dt, unsigned int number_of_items) {
     vector->symbols = (char**)safe_calloc(number_of_items, sizeof(char *));
     if (gnoll_errno) return;
 
-    for (unsigned int i = 0; i < number_of_items; i++) {
+    for (unsigned long longi = 0; i < number_of_items; i++) {
       vector->symbols[i] = (char*)safe_calloc(MAX_SYMBOL_LENGTH, sizeof(char));
       if (gnoll_errno) return;
     }
   }
 }
 
-void concat_symbols(char **arr1, unsigned int len1, char **arr2,
-                    unsigned int len2, char **new_arr) {
+void concat_symbols(char **arr1, unsigned long long len1, char **arr2,
+                    unsigned long long len2, char **new_arr) {
   /**
    * @brief Concatenates two 2D arrays of dice symbols
    * @param arr1
@@ -79,18 +80,18 @@ void concat_symbols(char **arr1, unsigned int len1, char **arr2,
     return;
   }
 
-  for (unsigned int i = 0; i != len1; i++) {
+  for (unsigned long long i = 0; i != len1; i++) {
     strcpy(new_arr[i], arr1[i]);
     // free(arr1[i]);
   }
-  for (unsigned int i = 0; i != len2; i++) {
-    unsigned int idx = len1 + i;
+  for (unsigned long long i = 0; i != len2; i++) {
+    unsigned long long idx = len1 + i;
     strcpy(new_arr[idx], arr2[i]);
     // free(arr2[i]);
   }
 }
 
-void pop(int *arr, unsigned int len, int value, int *new_arr) {
+void pop(long long *arr, unsigned long long len, long long value, long long *new_arr) {
   /**
    * @brief Removes a value from an array
    * @param arr
@@ -103,11 +104,11 @@ void pop(int *arr, unsigned int len, int value, int *new_arr) {
   }
 
   // This could be done in-place.
-  int seen = 0;
+  bool seen = false;
 
-  for (unsigned int i = 0; i != len; i++) {
+  for (unsigned long long i = 0; i != len; i++) {
     if (arr[i] == value && !seen) {
-      seen = 1;
+      seen = true;
       // Don't insert into new area.
     } else if (seen) {
       new_arr[i - 1] = arr[i];
@@ -117,7 +118,7 @@ void pop(int *arr, unsigned int len, int value, int *new_arr) {
   }
 }
 
-int contains(int *arr, unsigned int len, int value) {
+bool contains(int *arr, unsigned int len, int value) {
   /**
    * @brief Checks if a value exists in an array
    * @param arr
@@ -126,16 +127,16 @@ int contains(int *arr, unsigned int len, int value) {
    * @return true or false (1 or 0)
    */
   if (gnoll_errno) {
-    return 0;
+    return true;
   }
 
-  for (unsigned int i = 0; i != len; i++) {
-    if (arr[i] == value) return 1;
+  for (unsigned long long i = 0; i != len; i++) {
+    if (arr[i] == value) return true;
   }
-  return 0;
+  return false;
 }
 
-int min_in_vec(int *arr, unsigned int len) {
+long long min_in_vec(long long *arr, unsigned long long len) {
   /**
    * @brief Return the smallest value from an array
    * @param arr
@@ -146,14 +147,14 @@ int min_in_vec(int *arr, unsigned int len) {
     return 0;
   }
 
-  int lowest = INT_MAX;
-  for (unsigned int i = 0; i != len; i++) {
+  int lowest = LLONG_MAX;
+  for (unsigned long long i = 0; i != len; i++) {
     if (arr[i] < lowest) lowest = arr[i];
   }
   return lowest;
 }
 
-int max_in_vec(int *arr, unsigned int len) {
+long long max_in_vec(long long *arr, unsigned long long len) {
   /**
    * @brief Return the biggest value in an array
    * @param arr
@@ -164,14 +165,14 @@ int max_in_vec(int *arr, unsigned int len) {
     return 0;
   }
 
-  int highest = INT_MIN;
-  for (unsigned int i = 0; i != len; i++) {
+  int highest = LLONG_MIN;
+  for (unsigned long long i = 0; i != len; i++) {
     if (arr[i] > highest) highest = arr[i];
   }
   return highest;
 }
 void abs_vec(vec *x) {
-  for (unsigned int i = 0; i != x->length; i++) {
+  for (unsigned long long i = 0; i != x->length; i++) {
     int v = x->content[i];
     if (v < 0) {
       x->content[i] *= -1;
@@ -188,16 +189,16 @@ void print_vec(vec vector) {
     return;
   }
 
-  printf("Vector Size: %u\n", vector.length);
+  printf("Vector Size: %ull\n", vector.length);
   if (vector.dtype == NUMERIC) {
     printf("Vector Type: NUMERIC\n");
-    for (unsigned int i = 0; i != vector.length; i++) {
-      printf("\t%d\n", vector.content[i]);
+    for (unsigned long long i = 0; i != vector.length; i++) {
+      printf("\t%ll\n", vector.content[i]);
     }
   } else {
     printf("Vector Type: SYMBOLIC\n");
     printf("Symbols:\n");
-    for (unsigned int i = 0; i != vector.length; i++) {
+    for (unsigned long long i = 0; i != vector.length; i++) {
       printf("\t- %s\n", vector.symbols[i]);
     }
   }
@@ -226,12 +227,12 @@ void collapse_vector(vec *vector, vec *new_vector) {
     new_vector->has_source = false;
   } 
   else {
-    int c = 0;
-    for (unsigned int i = 0; i != vector->length; i++) {
+    long long c = 0;
+    for (unsigned long long i = 0; i != vector->length; i++) {
       c += vector->content[i];
     }
 
-    new_vector->content = (int*)safe_calloc(sizeof(int), 1);
+    new_vector->content = (long long*)safe_calloc(sizeof(long long), 1);
     if (gnoll_errno) return;
     new_vector->content[0] = c;
     new_vector->length = 1;
@@ -241,8 +242,8 @@ void collapse_vector(vec *vector, vec *new_vector) {
   return;
 }
 
-void keep_logic(vec *vector, vec *output_vector, unsigned int number_to_keep,
-                int keep_high) {
+void keep_logic(vec *vector, vec *output_vector, unsigned long long number_to_keep,
+                bool keep_high) {
   /**
    * @brief Collapses multiple Numeric dice to one value by summing
    * @param vector source (Freed at end)
@@ -254,7 +255,7 @@ void keep_logic(vec *vector, vec *output_vector, unsigned int number_to_keep,
     return;
   }
 
-  unsigned int available_amount = vector->length;
+  unsigned long long available_amount = vector->length;
 
   if (vector->dtype == SYMBOLIC) {
     printf(
@@ -275,12 +276,12 @@ void keep_logic(vec *vector, vec *output_vector, unsigned int number_to_keep,
 
     int *arr = vector->content;
     int *new_arr;
-    unsigned int length = vector->length;
+    unsigned long long length = vector->length;
 
     // while (number needed)
     //     Get Max/Min from vector
     //     Store in output
-    for (unsigned int i = 0; i != number_to_keep; i++) {
+    for (unsigned long long i = 0; i != number_to_keep; i++) {
       int m;
       if (keep_high) {
         m = max_in_vec(arr, length);
@@ -288,7 +289,7 @@ void keep_logic(vec *vector, vec *output_vector, unsigned int number_to_keep,
         m = min_in_vec(arr, length);
       }
       output_vector->content[i] = m;
-      new_arr = (int*)safe_calloc(sizeof(int), length - 1);
+      new_arr = (long long*)safe_calloc(sizeof(long long), length - 1);
       if (gnoll_errno) {
         return;
       }
@@ -310,49 +311,49 @@ void keep_logic(vec *vector, vec *output_vector, unsigned int number_to_keep,
 }
 
 void keep_lowest_values(vec *vector, vec *new_vector,
-                        unsigned int number_to_keep) {
+                        unsigned long long number_to_keep) {
   /**
    * @brief Keep the lowest values from a set of dice
    */
-  keep_logic(vector, new_vector, number_to_keep, 0);
+  keep_logic(vector, new_vector, number_to_keep, false);
 }
 void keep_highest_values(vec *vector, vec *new_vector,
-                         unsigned int number_to_keep) {
+                         unsigned long long number_to_keep) {
   /**
    * @brief Keep the Highest values from a set of dice
    */
-  keep_logic(vector, new_vector, number_to_keep, 1);
+  keep_logic(vector, new_vector, number_to_keep, true);
 }
 void drop_lowest_values(vec *vector, vec *new_vector,
-                        unsigned int number_to_keep) {
+                        unsigned long long number_to_keep) {
   /**
    * @brief Drop the lowest values from a set of dice
    */
-  int calc_keep = (int)vector->length - (int)number_to_keep;
+  long long calc_keep = (long long)vector->length - (long long)number_to_keep;
   if (calc_keep > 0) {
-    number_to_keep = (unsigned int)calc_keep;
+    number_to_keep = (unsigned long long)calc_keep;
   } else {
-    number_to_keep = (unsigned int)vector->length;
+    number_to_keep = (unsigned long long)vector->length;
   }
-  keep_logic(vector, new_vector, number_to_keep, 1);
+  keep_logic(vector, new_vector, number_to_keep, true);
 }
 void drop_highest_values(vec *vector, vec *new_vector,
-                         unsigned int number_to_keep) {
+                         unsigned long long number_to_keep) {
   /**
    * @brief Drop the highest values from a set of dice
    */
 
-  int calc_keep = (int)vector->length - (int)number_to_keep;
+  long long calc_keep = (long long)vector->length - (long long)number_to_keep;
   if (calc_keep > 0) {
-    number_to_keep = (unsigned int)calc_keep;
+    number_to_keep = (unsigned long long)calc_keep;
   } else {
-    number_to_keep = (unsigned int)vector->length;
+    number_to_keep = (unsigned long long)vector->length;
   }
   keep_logic(vector, new_vector, number_to_keep, 0);
 }
 
-void extract_symbols(char **symbols_list, char **result_symbols, int *indexes,
-                     unsigned int idx_length) {
+void extract_symbols(char **symbols_list, char **result_symbols, long long *indexes,
+                     unsigned long long idx_length) {
   /**
    * @brief Take symbols from indexed locations in an array and save to a new
    * location
@@ -366,13 +367,13 @@ void extract_symbols(char **symbols_list, char **result_symbols, int *indexes,
   }
 
   // Free up memory before overwriting (done in vec-init)
-  for (unsigned int i = 0; i != idx_length; i++) {
+  for (unsigned long long i = 0; i != idx_length; i++) {
     if (result_symbols[i]) {
       free(result_symbols[i]);
     }
   }
 
-  for (unsigned int i = 0; i != idx_length; i++) {
+  for (unsigned long long i = 0; i != idx_length; i++) {
     int index = indexes[i];
     result_symbols[i] = safe_strdup(symbols_list[index]);
   }
@@ -394,9 +395,9 @@ void filter(vec *dice, vec *cond, int comp_op, vec *output) {
     return;
   }
 
-  unsigned int success_idx = 0;
-  for (unsigned int i = 0; i != dice->length; i++) {
-    int v = dice->content[i];
+  unsigned long long success_idx = 0;
+  for (unsigned long long i = 0; i != dice->length; i++) {
+    long long v = dice->content[i];
     if (comp_op == IF_EVEN || comp_op == IF_ODD){
       if(check_condition_scalar(v, v, (COMPARATOR)comp_op)){
         output->content[success_idx] = v;
@@ -404,7 +405,7 @@ void filter(vec *dice, vec *cond, int comp_op, vec *output) {
       }
     }else{
 
-      int compare_to = cond->content[0];
+      long long compare_to = cond->content[0];
 
       if (check_condition_scalar(v, compare_to, (COMPARATOR)comp_op)) {
         output->content[success_idx] = v;
@@ -425,9 +426,9 @@ void filter_unique(vec *dice, vec *new_vec) {
     return;
   }
 
-  unsigned int tracker_idx = 0;
-  for (unsigned int i = 0; i != dice->length; i++) {
-    int v = dice->content[i];
+  unsigned long long tracker_idx = 0;
+  for (unsigned long long i = 0; i != dice->length; i++) {
+    long long v = dice->content[i];
 
     if (!contains(new_vec->content, new_vec->length, v)) {
       new_vec->content[tracker_idx] = v;
