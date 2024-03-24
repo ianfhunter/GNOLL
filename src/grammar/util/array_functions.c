15432105731296f4a969c811ba70cd5306ccc936
Ian Hunter <ianfhunter@gmail.com>
2024-03-24 00:20:36 +0000

    Update array_functions.c
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/util/array_functions.c
+++ src/grammar/util/array_functions.c
@@ -1,12 +1,12 @@
 
 #include "util/array_functions.h"
 
-int collapse(int * arr, unsigned int len){
+int collapse(int * arr, unsigned long long len){
     return sum(arr, len);
 }
 
-int sum(int * arr, unsigned int len){
+int sum(int * arr, unsigned long long len){
     int result = 0;
-    for(unsigned int i = 0; i != len; i++) result += arr[i];
+    for(unsigned long long i = 0; i != len; i++) result += arr[i];
     return result;
 }
