5c9ab89da4619f17c87ec9ec6bf7801cf7789eff
Ian Hunter <ianfhunter@gmail.com>
2024-03-24 00:19:55 +0000

    Update array_functions.h
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/util/array_functions.h
+++ src/grammar/util/array_functions.h
@@ -2,7 +2,7 @@
 #ifndef ARRAY_FN_HEADER
 #define ARRAY_FN_HEADER
 
-int sum(int* arr, unsigned int len);
-int collapse(int* arr, unsigned int len);
+int sum(long long* arr, unsigned long long len);
+int collapse(long long* arr, unsigned long long len);
 
 #endif
