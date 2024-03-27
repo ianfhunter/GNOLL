cbe50fb69de6232810985cb139ad569140c3a82d
Ian Hunter <ianfhunter@gmail.com>
2024-03-27 09:42:22 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/python/target.mk
+++ src/python/target.mk
@@ -7,8 +7,7 @@ python: all
 	rm -rf src/python/code/gnoll.egg-info/
 
 	cp -r build/  src/python/code/gnoll/c_build/
-	mkdir -p src/python/code/gnoll/c_includes/
-	rsync -av --progress src/grammar/ src/python/code/gnoll/c_includes/ \
+	rsync -avR --progress src/grammar/ src/python/code/gnoll/c_includes/ \
                --exclude creating gnoll-4.4.0/code/gnoll/c_includes/external/pcg-c/extras \
                --exclude creating gnoll-4.4.0/code/gnoll/c_includes/external/pcg-c/sample \
                --exclude creating gnoll-4.4.0/code/gnoll/c_includes/external/pcg-c/test-high \
