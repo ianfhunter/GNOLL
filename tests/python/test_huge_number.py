c5652e6c8ca1b2c72df3b7bfd5577da1f5bd4dd1
Ian Hunter <ianfhunter@gmail.com>
2024-03-29 11:12:53 +0000

    Update test_huge_number.py
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- tests/python/test_huge_number.py
+++ tests/python/test_huge_number.py
@@ -8,14 +8,30 @@ from util import roll
     "r, at_least",
     [
         ("1d922337203685477580", 10000000000000000)
-        # "1d" + "9" * 100,    # Too large for long long
-        # "1d" + "9" * 1000,
-        # "1d" + "9" * 10000,
-        # "1d" + "9" * 100000,
-        # "1d" + "9" * 10000000000000, # Too large for Python :)
     ],
 )
-def test_numbers(r, at_least):
+def test_longlong(r, at_least):
+    largest_value = 0
+    for x in range(300):
+        result, _ = roll(r)
+        largest_value = max(largest_value, result)
+        if largest_value > at_least:
+            break
+    assert largest_value > at_least
+
+
+@pytest.mark.skip(reason="Not Implemented yet")
+@pytest.mark.parametrize(
+    "r, at_least",
+    [
+        ("1d" + "9" * 100, int("1"*100),
+        ("1d" + "9" * 1000, int("1"*1000),
+        ("1d" + "9" * 10000, int("1"*10000),
+        ("1d" + "9" * 100000, int("1"*100000),
+        ("1d" + "9" * 10000000000000, int("1"*10000000000000),
+    ],
+)
+def test_ridiculously_big_numbers(r, at_least):
     largest_value = 0
     for x in range(300):
         result, _ = roll(r)
