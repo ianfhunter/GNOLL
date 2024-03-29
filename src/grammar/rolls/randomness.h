dd09dd548d66566a9d50040e7fe718a067bb246d
Ian Hunter <ianfhunter@gmail.com>
2024-03-29 12:01:34 +0000

    Update randomness.h
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/grammar/rolls/randomness.h
+++ src/grammar/rolls/randomness.h
@@ -1,7 +1,9 @@
 #ifndef __RANDOMNESS_H__
 #define __RANDOMNESS_H__
 
+#if USE_SECURE_RANDOM == 1
 long long arc4random_uniform64(long long upper_bound);
+#endif
 
 long long get_random_uniformly(void);
 double get_random_normally(double mean, double std);
