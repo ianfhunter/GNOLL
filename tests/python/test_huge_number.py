1dfac7c29c1cc506ccf68bc3f5f898b21c47b86d
Ian Hunter <ianfhunter@gmail.com>
2024-03-29 11:10:47 +0000

    Update test_huge_number.py
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- tests/python/test_huge_number.py
+++ tests/python/test_huge_number.py
@@ -5,16 +5,21 @@ from util import roll
 
 
 @pytest.mark.parametrize(
-    "r",
+    "r, at_least",
     [
-        "1d99999999999999999999999999999999",
-        "1d" + "9" * 100,
-        "1d" + "9" * 1000,
-        "1d" + "9" * 10000,
-        "1d" + "9" * 100000,
+        ("1d922337203685477580", 10000000000000000)
+        # "1d" + "9" * 100,    # Too large for long long
+        # "1d" + "9" * 1000,
+        # "1d" + "9" * 10000,
+        # "1d" + "9" * 100000,
         # "1d" + "9" * 10000000000000, # Too large for Python :)
     ],
 )
-def test_numbers(r):
-    result, _ = roll(r)
-    assert result not in ["1", 1]
+def test_numbers(r, at_least):
+    largest_value = 0
+    for x in range(300):
+        result, _ = roll(r)
+        largest_value = max(largest_value, result)
+        if largest_value > at_least:
+            break
+    assert largest_value > at_least
