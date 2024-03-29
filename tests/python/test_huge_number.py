e086cf1bc7501756fad92f9bedbcd6c9983a0060
Ian Hunter <ianfhunter@gmail.com>
2024-03-29 12:30:24 +0000

    Update test_huge_number.py
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- tests/python/test_huge_number.py
+++ tests/python/test_huge_number.py
@@ -10,7 +10,7 @@ from util import roll
 )
 def test_longlong(r, at_least):
     largest_value = 0
-    for x in range(300):
+    for _ in range(300):
         result, _ = roll(r)
         largest_value = max(largest_value, result)
         if largest_value > at_least:
@@ -24,14 +24,12 @@ def test_longlong(r, at_least):
     [
         ("1d" + "9" * 100, int("1" * 100)),
         ("1d" + "9" * 1000, int("1" * 1000)),
-        # ("1d" + "9" * 10000, int("1" * 10000)),
-        # ("1d" + "9" * 100000, int("1" * 100000)),
-        # ("1d" + "9" * 10000000000000, int("1" * 10000000000000)),
+        # Numbers larger than this start causing issues in python even if we dont run the test.
     ],
 )
 def test_ridiculously_big_numbers(r, at_least):
     largest_value = 0
-    for x in range(300):
+    for _ in range(300):
         result, _ = roll(r)
         largest_value = max(largest_value, result)
         if largest_value > at_least:
