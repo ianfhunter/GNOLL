65447deaad92099dc2c3c3a07c8657559fe4ff78
Ian Hunter <ianfhunter@gmail.com>
2024-03-30 15:13:30 +0000

    Update target.mk
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>

--- src/js/target.mk
+++ src/js/target.mk
@@ -50,5 +50,5 @@ jsbundle: jsweb
 	yarn --cwd ./src/js run webpack-cli b
 
 jspcg:
-	emcc src/grammar/external/pcg-c/src/pcg-rngs-64.c src/grammar/external/pcg-c/include/pcg_variants.h 
+	emcc src/grammar/external/pcg-c/src/pcg-rngs-64.c -I src/grammar/external/pcg-c/include/pcg_variants.h 
 	
