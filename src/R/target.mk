091d228d7cc2775e2d33b54aaa44b425b4e59676
Ian Hunter <ianfhunter@gmail.com>
2025-05-04 06:52:31 +0100

    [Snyk] Security upgrade setuptools from 40.5.0 to 78.1.1 (#511)
    * fix: reqs/requirements.txt to reduce vulnerabilities
    
    
    The following vulnerabilities are fixed by pinning transitive dependencies:
    - https://snyk.io/vuln/SNYK-PYTHON-SETUPTOOLS-9964606
    
    * Update publish_JOSS.yml
    
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>
    
    * Update publish_JOSS.yml
    
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>
    
    * Update publish_docs.yml
    
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>
    
    * Update target.mk
    
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>
    
    * Update target.mk
    
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>
    
    * Update test_packages.yml
    
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>
    
    * Update test_packages.yml
    
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>
    
    * Update test_packages.yml
    
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>
    
    * Update setup.cfg
    
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>
    
    ---------
    
    Signed-off-by: Ian Hunter <ianfhunter@gmail.com>
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>

--- src/R/target.mk
+++ src/R/target.mk
@@ -2,5 +2,5 @@
 
 r: clean yacc lex compile $(OBJECTS)
 	mkdir -p build/r/
-	$(CC) -shared -o build/r/dice.so $(OBJECTS) $(ARC4RANDOM) -lR  -lpcg_random -L$(PCG_SRC) 
+	$(CC) -shared -o build/r/dice.so $(OBJECTS) $(ARC4RANDOM)  $(shell R CMD config --ldflags)  -lpcg_random -L$(PCG_SRC) 
 	Rscript src/R/main.r
